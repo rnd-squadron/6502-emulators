6bcce9ae1a09a3600a091c571087c4e9c53ccef4
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2022-11-20 23:47:55 +0300

    feature: byte-to-instruction converter (adc)

--- rust/src/instructions.rs
+++ rust/src/instructions.rs
@@ -1,4 +1,5 @@
 #![allow(unused)]
+
 enum Instruction {
     // Common Load/Store opcodes
     Lda,
@@ -7,11 +8,13 @@ enum Instruction {
     Sta,
     Stx,
     Sty,
+
     // Common Transfer opcodes
     Tay,
     Tya,
     Tax,
     Txa,
+
     // Common Math opcodes
     // A/S - add/substract, I/D - inc/decrement, A/L - arithmetic/logical shift right
     Adc,
@@ -25,12 +28,34 @@ enum Instruction {
     Dex,
     Asl,
     Lsr,
+
     // Common Comparison opcodes
     Cmp,
     Cpx,
     Cpy,
+
     // Common Control Flow opcodes
     Jmp,
     Beq,
     Bne,
+}
+
+impl Instruction {
+    fn from_byte(byte: u8) -> Instruction { 
+        match(byte) { 
+            0x69 => Instruction::Adc,
+            _ => panic!("Opcode not found! Opcode: {:x}", byte)
+        }
+    }
+} 
+
+#[cfg(test)]
+mod opcode_test { 
+    use super::Instruction;
+
+    #[test]
+    #[should_panic]
+    fn instruction_from_byte_test() { 
+        Instruction::from_byte(0xff);
+    }
 }
